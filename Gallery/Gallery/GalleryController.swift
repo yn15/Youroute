6832fe06cdfd23e67175441767d3e3e03bb0b34a
loveminpic <minji0479@naver.com>
2020-08-19 22:36:06 +0900

    set up google login

--- Gallery/Gallery/GalleryController.swift
+++ Gallery/Gallery/GalleryController.swift
@@ -21,7 +21,7 @@ class GalleryController: UIViewController {
         }
     }
     
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ Gallery/Gallery/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TEST_FIREBASE
+//
+//  Created by mac on 2020/06/10.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+
+class ViewController: UIViewController, GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+    }
+    // 구글 버튼 연결 부분
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    
+    @IBAction func test(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance().signIn()
+        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
