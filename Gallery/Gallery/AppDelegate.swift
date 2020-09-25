8ab372ebb9fd11342ad6f6eb1dfd8bb3d6dc09fc
loveminpic <minji0479@naver.com>
2020-09-26 02:33:44 +0900

    구글 로그인 부분 완성 및 이메일 로그인 작업중
    파이어 베이스 디비에 사용자들 아이디 저장되게 할 예정.

--- Gallery/Gallery/AppDelegate.swift
+++ Gallery/Gallery/AppDelegate.swift
@@ -44,15 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate{
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    //facebook
-//    func application( _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool {
-//        ApplicationDelegate.shared.application( app, open: url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String, annotation: options[UIApplication.OpenURLOptionsKey.annotation] ) } }
-//
-//
-//
-    
-    
-    
+ 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
--- Gallery/Gallery/ViewController.swift
+++ Gallery/Gallery/ViewController.swift
@@ -14,18 +14,80 @@ import GoogleSignIn
 class ViewController: UIViewController {
     
 @IBOutlet var signInButton : GIDSignInButton!
-
+// email sign in1
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "YouRoute Log In"
+        label.font = .systemFont(ofSize:24, weight :.semibold )
+        return label
+        }()
+    
+    private let emailField: UITextField = {
+        let emailField = UITextField()
+        emailField.placeholder = "Email Address"
+        emailField.layer.borderWidth = 1
+        emailField.layer.backgroundColor = UIColor.white.cgColor
+        return emailField
+    }()
+    
+    private let passField: UITextField = {
+        let passField  = UITextField()
+        passField.placeholder = "Password"
+        passField.layer.borderWidth = 1
+        passField.isSecureTextEntry = true
+        passField.layer.backgroundColor = UIColor.white.cgColor
+        return passField
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemGreen
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Continue", for : .normal)
+           return button
+       }()
+// email sign in 1 end
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if GIDSignIn.sharedInstance()?.currentUser != nil {
-
-        } else {
-            GIDSignIn.sharedInstance()?.signIn()
-        }
-
-
+        // googleSign in
+       
         GIDSignIn.sharedInstance()?.presentingViewController = self
-
+        
+        // email sign in
+        
+        view.addSubview(label)
+        view.addSubview(emailField)
+        view.addSubview(passField)
+        view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        label.frame = CGRect(x: 0, y: 100, width: view.frame.size.width, height: 80)
+        emailField.frame = CGRect(x: 20,
+                                  y: label.frame.origin.y+label.frame.size.height+10,
+                                  width: view.frame.size.width - 40,
+                                  height: 50)
+         
+        passField.frame = CGRect(x: 20,
+                                 y: emailField.frame.origin.y+emailField.frame.size.height+10,
+                                 width: view.frame.size.width - 40,
+                                 height: 50)
+        button.frame = CGRect(x: 20,
+                              y: passField.frame.origin.y+passField.frame.size.height+10,
+                              width: view.frame.size.width - 40 ,
+                              height: 80)
+        
+    }
+    
+    @objc private func didTapButton(){
+        print("Continue button tap")
+        
     }
 }
