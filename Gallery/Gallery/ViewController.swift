63ef1ba8c9db19fd20496015472fcb23efbc532c
loveminpic <minji0479@naver.com>
2020-09-26 15:49:45 +0900

    done with Auth!!!! finally!!

--- Gallery/Gallery/ViewController.swift
+++ Gallery/Gallery/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 import GoogleSignIn
-
+import FirebaseAuth
+import Firebase
 
 
 class ViewController: UIViewController {
@@ -20,13 +21,13 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.textAlignment = .center
         label.text = "YouRoute Log In"
-        label.font = .systemFont(ofSize:24, weight :.semibold )
+        label.font = .systemFont(ofSize:24, weight :.semibold)
         return label
         }()
     
     private let emailField: UITextField = {
         let emailField = UITextField()
-        emailField.placeholder = "Email Address"
+        emailField.placeholder = " Email Address"
         emailField.layer.borderWidth = 1
         emailField.layer.backgroundColor = UIColor.white.cgColor
         return emailField
@@ -34,7 +35,7 @@ class ViewController: UIViewController {
     
     private let passField: UITextField = {
         let passField  = UITextField()
-        passField.placeholder = "Password"
+        passField.placeholder = " Password"
         passField.layer.borderWidth = 1
         passField.isSecureTextEntry = true
         passField.layer.backgroundColor = UIColor.white.cgColor
@@ -43,7 +44,7 @@ class ViewController: UIViewController {
     
     private let button: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .systemGreen
+        button.backgroundColor = .systemGray
         button.setTitleColor(.white, for: .normal)
         button.setTitle("Continue", for : .normal)
            return button
@@ -62,7 +63,7 @@ class ViewController: UIViewController {
         view.addSubview(emailField)
         view.addSubview(passField)
         view.addSubview(button)
-        
+        view.backgroundColor = .systemGray2
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         
     }
@@ -88,6 +89,64 @@ class ViewController: UIViewController {
     
     @objc private func didTapButton(){
         print("Continue button tap")
+        guard let email  = emailField.text, !email.isEmpty,
+            let password = passField.text, !password.isEmpty else {
+                print("Missing Field data")
+                return
+        }
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {[weak self] authResult, error in
+            guard let strongSelf = self else{
+                return
+            }
+            
+            guard error == nil else {
+                //show account creation
+                strongSelf.showCreateAccount(email: email, password: password)
+                return
+            }
+            print("You have Sign In")
+            strongSelf.label.isHighlighted = true
+            strongSelf.emailField.isHighlighted = true
+            strongSelf.passField.isHighlighted = true
+            strongSelf.button.isHighlighted = true
+            
+        })
+    }
+    
+    func showCreateAccount(email:String,password : String){
+        let alert = UIAlertController(title: "Creat account",
+                                      message: "Would you like to create an account",
+                                      preferredStyle: .alert )
         
+        alert.addAction(UIAlertAction(title:  "Continue",
+                                      style: .default,
+                                      handler: {_ in
+                                        
+                                        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { [weak self] authResult, error in
+                                            
+                                            guard let strongSelf = self else{
+                                                    return
+                                                }
+                                                
+                                                guard error == nil else {
+                                                    //show account creation
+                                                    print("Account create failed")
+                                                    return
+                                                }
+                                                print("You have Sign In")
+                                                strongSelf.label.isHighlighted = true
+                                                strongSelf.emailField.isHighlighted = true
+                                                strongSelf.passField.isHighlighted = true
+                                                strongSelf.button.isHighlighted = true
+                                                
+                                            })
+
+        }))
+        alert.addAction(UIAlertAction(title:  "Cancel",
+                                      style: .cancel,
+                                      handler: {_ in
+                                        
+        }))
+        present(alert, animated: true)
     }
 }
